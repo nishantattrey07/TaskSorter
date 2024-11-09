0d005d45fc1f3affa105d1ae49d073f6d02dfffc
nishantattrey07 <nishantattrey@gmail.com>
2024-11-09 16:46:17 +0530

    Refactor user model fields and enhance signup route with JWT authentication

--- backend/dist/routes/user.js
+++ backend/dist/routes/user.js
@@ -1,21 +1,67 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const Codes_1 = require("../schemas,types/Codes");
+const client_1 = require("@prisma/client");
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
+const prisma = new client_1.PrismaClient();
 const userSchema_1 = require("../schemas,types/userSchema");
 router.get("/", (req, res) => {
     res.send("User route");
 });
-router.post("/signup", (req, res) => {
+router.post("/signup", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { username, firstName, lastName, password, email } = req.body;
     const resultParse = userSchema_1.userSignUpSchema.safeParse({ username, firstName, lastName, password, email });
-    console.log(resultParse);
-    console.log(resultParse.error);
-    res.send("done");
-});
+    if (!resultParse.success) {
+        console.log(resultParse.error);
+        res.status(Codes_1.ResponseStatus.BadRequest).json({
+            message: "Invalid data"
+        });
+        return;
+    }
+    try {
+        const response = yield prisma.user.create({
+            data: {
+                username,
+                firstName,
+                lastName,
+                password,
+                email,
+            },
+        });
+        console.log("this is response", response);
+        if (!process.env.JWT_SECRET) {
+            throw new Error("JWT_SECRET is not defined");
+        }
+        const token = jsonwebtoken_1.default.sign({ username: username }, process.env.JWT_SECRET);
+        res.status(Codes_1.ResponseStatus.Created).json({
+            message: "User created successfully",
+            token: token
+        });
+        console.log("this is token", token);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(Codes_1.ResponseStatus.InternalServerError).json({
+            message: "Something went wrong"
+        });
+    }
+}));
 router.post("/signin", (req, res) => {
     const { email, password } = req.body;
     const resultParse = userSchema_1.userSignInSchema.safeParse({ email, password });
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,18 +1,60 @@
 import express from "express";
 const router = express.Router();
+import jwt from "jsonwebtoken";
+import { ResponseStatus } from "../schemas,types/Codes";
+import { PrismaClient } from "@prisma/client";
+import { config } from "dotenv";
+config();
+
+
+const prisma = new PrismaClient();
+
 import { userSignUpSchema,userSignInSchema } from "../schemas,types/userSchema";
 router.get("/", (req, res) => {
   res.send("User route");
 });
 
 
-router.post("/signup", (req, res) => {
-    const { username, firstName, lastName, password, email } = req.body;
-    
-    const resultParse = userSignUpSchema.safeParse({ username, firstName, lastName, password, email });
-    console.log(resultParse);
+router.post("/signup", async(req, res) => {
+  const { username, firstName, lastName, password, email } = req.body;
+
+  const resultParse = userSignUpSchema.safeParse({ username, firstName, lastName, password, email });
+
+  if (!resultParse.success) {
     console.log(resultParse.error);
-    res.send("done")
+    res.status(ResponseStatus.BadRequest).json({
+      message: "Invalid data"
+     });
+    return;
+  }
+  try {
+    const response = await prisma.user.create({
+      data: {
+        username,
+        firstName,
+        lastName,
+        password,
+        email,
+      },
+      
+    });
+    console.log("this is response",response);
+    if (!process.env.JWT_SECRET) {
+      throw new Error("JWT_SECRET is not defined");
+    }
+    const token = jwt.sign({ username: response.username }, process.env.JWT_SECRET);
+    res.status(ResponseStatus.Created).json({
+      message: "User created successfully",
+      token:token
+    });
+    return;
+   }
+  catch (error) {
+    console.log(error);
+    res.status(ResponseStatus.InternalServerError).json({
+      message: "Something went wrong"
+    })
+  }
 })
 
 router.post("/signin", (req, res) => {
